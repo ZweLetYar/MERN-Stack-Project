a38d37a3d9b88584bcfb4dc28c9f501e3c5145af
Zwe Let Yar <letyarzwe@gmail.com>
2025-06-04 23:19:31 +0630

    Filter,Sort,Paging Completed

--- server/index.js
+++ server/index.js
@@ -5,20 +5,58 @@ const mongojs = require("mongojs");
 const db = mongojs("mongodb://127.0.0.1:27017/travel", ["records"]);
 
 const bodyParser = require("body-parser");
+const qs = require("qs");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/api/records", (req, res) => {
-  db.records.find((err, data) => {
-    if (err) {
-      return res.sendStatus(500);
-    } else {
-      return res.status(200).json({
-        meta: { total: data.length },
-        data,
+  const options = qs.parse(req.query);
+  const sort = options.sort || {};
+  const filter = options.filter || {};
+  const limit = 4;
+  const page = parseInt(options.page);
+  const skip = (page - 1) * limit;
+
+  for (i in sort) {
+    sort[i] = parseInt(sort[i]);
+  }
+
+  db.records.count(filter, (err, totalCount) => {
+    if (err) return res.sendStatus(500);
+    db.records
+      .find(filter)
+      .sort(sort)
+      .skip(skip)
+      .limit(limit, (err, data) => {
+        if (err) {
+          return res.sendStatus(500);
+        } else {
+          const totalPages = Math.ceil(totalCount / limit);
+          const queryString = qs.stringify({ sort, filter }, { encode: false });
+          const baseUrl = `/api/records?${queryString}`;
+
+          const links = {
+            self: `${baseUrl}&page=${page}`,
+            first: `${baseUrl}&page=1`,
+            last: `${baseUrl}&page=${totalPages}`,
+          };
+
+          if (page > 1) {
+            links.prev = `${baseUrl}&page=${page - 1}`;
+          }
+
+          if (page < totalPages) {
+            links.next = `${baseUrl}&page=${page + 1}`;
+          }
+
+          return res.status(200).json({
+            meta: { total: data.length, skip, limit, filter, sort, page },
+            data,
+            links,
+          });
+        }
       });
-    }
   });
 });
 
